38ee3bc8829dd39221bc3ce50faf49bd01aabcfd
sastrapradana <bp846622@gmail.com>
2024-05-17 20:40:51 +0700

    study next & ts

---
+++ src/components/products/Button-cart.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+const ButtonCart = ({ description }: { description: string }) => {
+  const handleClick = () => {
+    alert(description);
+  };
+
+  return (
+    <button
+      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      onClick={handleClick}
+    >
+      Add to cart
+    </button>
+  );
+};
+
+export default ButtonCart;
